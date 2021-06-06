be298f9fbb6231d81df62a4002d4431b24756861
Thomas Toye <thomas@toye.io>
2021-06-06 23:26:53 +0200

    Working on parsing CACH bursts

--- shell.nix
+++ shell.nix
@@ -1,42 +1,21 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ nixpkgs ? import (fetchTarball https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz) {} }:
+with nixpkgs;
 
 let
-  commpy = pkgs.python38Packages.buildPythonPackage rec {
-      pname = "scikit-commpy";
-      version = "0.6.0";
-
-      src = pkgs.python38Packages.fetchPypi{
-        inherit version;
-        inherit pname;
-        sha256 = "1n5i5fdglg9s1lyr2rx72h742ighl97llznw3ksbazmxrcs7zv1r";
-      };
-
-      doCheck = false;
-
-      buildInputs = [
-        pkgs.python38Packages.numpy
-        pkgs.python38Packages.scipy
-        pkgs.python38Packages.matplotlib
-        pkgs.python38Packages.nose
-        pkgs.python38Packages.sympy
-      ];
-    };
-  customPython = pkgs.python38.buildEnv.override {
+  customPython = python39.buildEnv.override {
     extraLibs = [
-      pkgs.python38Packages.notebook
-      pkgs.python38Packages.jupyter
-      pkgs.python38Packages.scipy
-      pkgs.python38Packages.numpy
-      pkgs.python38Packages.matplotlib
-      commpy
-      pkgs.python38Packages.nbdime
+      python39Packages.notebook
+      python39Packages.jupyter
+      python39Packages.scipy
+      python39Packages.numpy
+      python39Packages.matplotlib
+      # python39Packages.nbdime
     ];
   };
 in
   pkgs.mkShell {
     buildInputs = [
       customPython
-      pkgs.git-lfs
+      git-lfs
     ];
   }
-
