98de42a2c69aa0e310efbaf4506604e6abe2fcdd
Thomas Toye <thomas@toye.io>
2021-06-21 09:02:01 +0200

    Add start.py for easy manual testing

---
+++ dmrpy/start.py
@@ -0,0 +1,15 @@
+from dmrpy.pdu.cach_burst import CachBurst
+from dmrpy.layer_1.symbol_stream_to_packets import symbol_stream_to_packets
+from dmrpy.fixtures.symbol_stream import (
+    EXAMPLE_SYMBOL_STREAM_SHORT,
+)
+
+print(
+    "\n".join(
+        [
+            str(CachBurst.create_from_burst_binary(data))
+            for (kind, data) in symbol_stream_to_packets(EXAMPLE_SYMBOL_STREAM_SHORT)
+            if kind == "cach"
+        ]
+    )
+)
