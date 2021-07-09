71a60d9bf45ac40b9e8dac858c61dbff3a70b8a1
Thomas Toye <thomas@toye.io>
2021-07-09 17:15:34 +0200

    Derive symbol stream from source file

--- dmrpy/start.py
+++ dmrpy/start.py
@@ -1,25 +1,25 @@
-# from dmrpy.pdu.voice_burst import VoiceBurst
-# from dmrpy.pdu.cach_burst import CachBurst
-# from dmrpy.layer_1.symbol_stream_to_packets import symbol_stream_to_packets
-# from dmrpy.fixtures.symbol_stream import (
-#     EXAMPLE_SYMBOL_STREAM_SHORT,
-# )
-
-from dmrpy.fixtures.fixtures import FIXTURE, FIXTURE_SAMPLE_RATE
+import numpy as np
+from dmrpy.pdu.voice_burst import VoiceBurst
+from dmrpy.pdu.cach_burst import CachBurst
+from dmrpy.layer_1.symbol_stream_to_packets import symbol_stream_to_packets
 from dmrpy.layer_1.audio_to_symbols import audio_stream_to_symbols
-import plotext
+from dmrpy.fixtures.fixtures import FIXTURE, FIXTURE_SAMPLE_RATE
 
-result = audio_stream_to_symbols(FIXTURE, FIXTURE_SAMPLE_RATE)
-plotext.hist(result[0:4800], bins=100)
-plotext.show()
+symbol_stream = np.array(
+    [
+        symbol["symbol"]
+        for symbol in audio_stream_to_symbols(FIXTURE, FIXTURE_SAMPLE_RATE)
+    ]
+)
 
-
-# print(
-#     "\n".join(
-#         [
-#             str(VoiceBurst.create_from_burst_binary(data))
-#             for (kind, data) in symbol_stream_to_packets(EXAMPLE_SYMBOL_STREAM_SHORT)
-#             if kind != "cach"
-#         ]
-#     )
-# )
+print(
+    "\n".join(
+        [
+            str(start_index).zfill(8)
+            + "  "
+            + str(VoiceBurst.create_from_burst_binary(data))
+            for (kind, data, start_index) in symbol_stream_to_packets(symbol_stream)
+            if kind != "cach"
+        ]
+    )
+)
