1ebf0401729728ec33c1720d47a16104392f456f
Thomas Toye <thomas@toye.io>
2021-09-10 14:48:55 +0200

    Full LC experimentation

--- dmrpy/pdu/full_lc.py
+++ dmrpy/pdu/full_lc.py
@@ -1,4 +1,9 @@
+from dmrpy.util.numpy_bit_array_to_int import numpy_bit_array_to_int
+from dmrpy.util.to_numpy_bit_array import to_numpy_bit_array
+import numpy as np
+from dmrpy.pdu.voice_burst import VoiceBurst
 from dmrpy.parity.five_bit_checksum import calculate_five_bit_checksum
+from typing import List
 
 
 class FullLC:
@@ -31,7 +36,7 @@ class FullLC:
             + self.data
         )
 
-    def create_from_embedded_signalling_binary(binary, checksum):
+    def create_from_embedded_signalling_binary(binary: int, checksum: int):
         # 77 bits
         return FullLC(
             pf=(binary >> 71) & 0x1,
@@ -40,6 +45,58 @@ class FullLC:
             fid=(binary >> 56) & 0xFF,
             data=binary & 0xFFFFFFFFFFFFFF,
             cs_5bit=checksum,
+        )
+
+    def create_from_superframe(superframe: List[VoiceBurst]):
+        assert len(superframe) == 6
+        assert superframe[0].embedded_signalling == None  # Should be SYNC
+
+        # See ETSI TS 102 361-1 Section 7.1.3.1
+        # Voice superframe example:
+        # A: (Sync)
+        # B:  lcss=0x1 - 0x4e0f0606
+        # C:  lcss=0x3 - 0x17110047
+        # D:  lcss=0x3 - 0x0c03181b
+        # E:  lcss=0x2 - 0x175a0f4e
+        # F: (lcss=0x0 - 0x00000000)
+
+        # Embedded signallings for frames B-E
+        embedded_signallings = [frame.embedded_signalling for frame in superframe[1:5]]
+
+        # Perform deinterleaving, see B.2.1
+        # We have 4 embedded bursts => N=8 => 7*LC info + 1*simple parity check
+        # The BPTC encoding matrix has 8 rows, 16 columns (= 128 bits)
+        # The transmit matrix has      4 rows, 32 columns (= 128 bits)
+
+        transmit_matrix = np.array(
+            [to_numpy_bit_array(x, 32) for x in embedded_signallings]
+        )
+        bptc_encode_matrix = transmit_matrix.reshape(16, 8).transpose()
+
+        lc = np.concatenate(
+            (
+                bptc_encode_matrix[0][0:11],
+                bptc_encode_matrix[1][0:11],
+                bptc_encode_matrix[2][0:10],
+                bptc_encode_matrix[3][0:10],
+                bptc_encode_matrix[4][0:10],
+                bptc_encode_matrix[5][0:10],
+                bptc_encode_matrix[6][0:10],
+            )
+        )
+
+        checksum_bits = np.array(
+            [
+                bptc_encode_matrix[2][10],
+                bptc_encode_matrix[3][10],
+                bptc_encode_matrix[4][10],
+                bptc_encode_matrix[5][10],
+                bptc_encode_matrix[6][10],
+            ]
+        )
+
+        return FullLC.create_from_embedded_signalling_binary(
+            numpy_bit_array_to_int(lc), numpy_bit_array_to_int(checksum_bits)
         )
 
     def checksum_matches(self):
--- dmrpy/pdu/full_lc_test.py
+++ dmrpy/pdu/full_lc_test.py
@@ -1,9 +1,35 @@
+from dmrpy.util.numpy_bit_array_to_int import numpy_bit_array_to_int
+from dmrpy.pdu.voice_burst import VoiceBurst
 from dmrpy.pdu.full_lc import FullLC
 import numpy as np
 from dmrpy.util.to_numpy_bit_array import to_numpy_bit_array
 from numpy.testing import assert_array_equal
 
 # TODO This file is just experimentation, clean up
+
+VOICE_SUPERFRAME = [
+    # Data head (7.1.1): 0x2B6004101F842DD00DF07D41046DFF57D75DF5DE30152E2070B20F803F88C695E2
+    # See 7.1.3: A: Sync, B-E: Link Control, F: Reverse Channel Opportunity
+    VoiceBurst.create_from_burst_binary(
+        0xB9E881526173002A6BB9E881526755FD7DF75F7173002A6BB9E881526173002A6A
+    ),  # A
+    VoiceBurst.create_from_burst_binary(
+        0xB9E881526173002A6BB9E881526134E0F060691173002A6BB9E881526173002A6A
+    ),  # B
+    VoiceBurst.create_from_burst_binary(
+        0xB9E881526173002A6BB9E881526171711004774173002A6BB9E881526173002A6A
+    ),  # C
+    VoiceBurst.create_from_burst_binary(
+        0xB9E881526173002A6B954BE6500170C03181B74310B00777A6C6CB53732789483A
+    ),  # D
+    VoiceBurst.create_from_burst_binary(
+        0x865AE7617555B50601B758E665115175A0F4E07124815001FFF5A337706128A7CA
+    ),  # E
+    VoiceBurst.create_from_burst_binary(
+        0xEEE7817574614DF2FFCCF4A05511100000000E243059E7F9E908A0756202CCD622
+    ),  # F
+    # Voice term (7.1.2): 0x2B0F04C41F342DA80D807DE104ADFF57D75DF5D965012D1877D203C03788DF95D1
+]
 
 # Voice superframe example:
 # A: (Sync)
@@ -12,6 +38,81 @@ from numpy.testing import assert_array_equal
 # D:  lcss=0x3 - 0x0c03181b
 # E:  lcss=0x2 - 0x175a0f4e
 # F: (lcss=0x0 - 0x00000000)
+
+EXPECTED_LC = [
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    1,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    1,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    1,
+    1,
+    0,
+    0,
+    0,
+    0,
+    1,
+    1,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    1,
+    0,
+    1,
+    1,
+    1,
+    1,
+    1,
+    0,
+    0,
+    1,
+    1,
+    0,
+    1,
+    1,
+    1,
+    1,
+    1,
+    0,
+    0,
+    1,
+    0,
+    1,
+]
 
 # See 7.1.3.1
 
@@ -82,80 +183,7 @@ def extract_checksum_bits(a):
 def test_extract_lc():
     assert_array_equal(
         extract_lc(bptc_encode_matrix),
-        [
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            1,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            1,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            1,
-            1,
-            0,
-            0,
-            0,
-            0,
-            1,
-            1,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            1,
-            0,
-            1,
-            1,
-            1,
-            1,
-            1,
-            0,
-            0,
-            1,
-            1,
-            0,
-            1,
-            1,
-            1,
-            1,
-            1,
-            0,
-            0,
-            1,
-            0,
-            1,
-        ],
+        EXPECTED_LC,
     )
 
 
@@ -176,6 +204,33 @@ def test_full_lc_create_from_binary():
     assert lc.data == 0x20000C302F9BE5
     assert lc.raw() == 0x1020000C302F9BE5
     assert lc.checksum_matches()
+
+
+def test_create_from_superframe():
+    lc = FullLC.create_from_superframe(VOICE_SUPERFRAME)
+
+    # assert VOICE_SUPERFRAME[1].embedded_signalling == 0x4e0f0606
+    # assert VOICE_SUPERFRAME[2].embedded_signalling == 0x17110047
+    # assert VOICE_SUPERFRAME[3].embedded_signalling == 0x0c03181b
+    # assert VOICE_SUPERFRAME[4].embedded_signalling == 0x175a0f4e
+
+    # A: (Sync)
+    # B:  lcss=0x1 - 0x4e0f0606
+    # C:  lcss=0x3 - 0x17110047
+    # D:  lcss=0x3 - 0x0c03181b
+    # E:  lcss=0x2 - 0x175a0f4e
+    # F: (lcss=0x0 - 0x00000000)
+
+    # assert numpy_bit_array_to_int(EXPECTED_LC) == 0
+
+    assert lc.pf == 0
+    assert lc.reserved == 0
+    assert lc.flco == 0
+    assert lc.fid == 0x20
+    assert lc.cs_5bit == 0x18
+    assert lc.data == 0x400018605F37CA
+    assert lc.raw() == 0x20400018605F37CA
+    # assert lc.checksum_matches() TODO
 
 
 # Now we need to reshape the "transmit matrix" (actually the received matrix in our case)
